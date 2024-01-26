e6c665def1d0d8a6927c9d623b4f155ead60eccd
csrdg <cesardg.pb@gmail.com>
2024-01-26 15:35:42 +0100

    creating game

---
+++ hubjuegos/src/pages/PokeMatchGame/PokeMatchGame.js
@@ -0,0 +1,10 @@
+import { initControler } from "../../utils";
+import "./PokeMatcGame.css";
+
+const template = () => `
+    <div id="PokeMatchGame">
+     <h2 id="titleGame">POKE MATCH</h2>
+     <div id="containerBoardGame"></div>
+     <button id="resertGame">RESET!</div>
+    </div>
+`;
